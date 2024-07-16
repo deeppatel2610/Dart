3961806059f0946a67d8dafc6bf859c4b7df934e
Patel Deep <156797002+deeppatel2610@users.noreply.github.com>
2024-07-16 23:31:19 +0530

    Add files via upload

---
+++ OOP Architecture/named parameter/main.dart
@@ -0,0 +1,12 @@
+int sumList({required List<int> numbers}) {
+  int sum = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+void main() {
+  List<int> myList = [11, 22, 33, 44, 55];
+  print(sumList(numbers: myList)); // Output: 15
+}
