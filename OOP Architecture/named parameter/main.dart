b8b41cf79a0995d3838f67e8b57a568f07857352
Patel Deep <156797002+deeppatel2610@users.noreply.github.com>
2024-07-17 15:36:01 +0530

    Update main.dart

--- OOP Architecture/named parameter/main.dart
+++ OOP Architecture/named parameter/main.dart
@@ -7,6 +7,6 @@ int sumList({required List<int> numbers}) {
 }
 
 void main() {
-  List<int> myList = [11, 22, 33, 44, 55];
+  List<int> myList = [1,2,3,4,5,6,7,8,9,10];
   print(sumList(numbers: myList)); // Output: 15
 }
